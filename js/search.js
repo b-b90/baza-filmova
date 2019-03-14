6921592bf75c58be311235b7eb34529f4e611e78
bonadea88 <misic.anja@yahoo.com>
2019-03-14 17:38:01 +0100

    sortiranje

--- js/search.js
+++ js/search.js
@@ -4,6 +4,7 @@ fetch('https://baza-filmova.herokuapp.com/filmovi/ ')
 
     let sablon = '';
 
+    // ispisivanje svih objekata
     for(let i = 0; i < data.length; i++) {
       sablon += `<div class='col-md-3'>
                     <h4>${data[i].naziv}</h4>
@@ -13,7 +14,7 @@ fetch('https://baza-filmova.herokuapp.com/filmovi/ ')
 
     $('#filmovi').html(sablon);
 
-
+    // pretraga po nazivu
     $('#pretraga').on('keyup', function() {
         
     let pretraga = '';
@@ -28,5 +29,133 @@ fetch('https://baza-filmova.herokuapp.com/filmovi/ ')
             }
         }
         $('#filmovi').html(pretraga);
+    })
+
+    // sortiranje od najstarijeg
+    $('#odNajstarijeg').on('click', function() {
+
+      const godine = [];
+
+      for(i = 0; i < data.length; i++) {
+        godine.push(data[i]);
+      }
+
+      function compare(a,b) {
+        if (a.godina < b.godina)
+          return -1;
+        if (a.godina > b.godina)
+          return 1;
+        return 0;
+      }
+      
+      godine.sort(compare);
+
+      let sablon = '';
+
+      for(let i = 0; i < godine.length; i++) {
+        sablon += `<div class='col-md-3'>
+                      <h4>${godine[i].naziv}</h4>
+                      <h4>${godine[i].godina}</h4>
+                      <img src='${godine[i].slika}'>
+                    </div>`;
+      }
+
+      $('#filmovi').html(sablon);
+    })
+
+    // sortiranje od najnovijeg
+    $('#odNajnovijeg').on('click', function() {
+
+      const godine = [];
+
+      for(i = 0; i < data.length; i++) {
+        godine.push(data[i]);
+      }
+
+      function compare(a,b) {
+        if (a.godina < b.godina)
+          return -1;
+        if (a.godina > b.godina)
+          return 1;
+        return 0;
+      }
+      
+      godine.sort(compare);
+
+      let sablon = '';
+
+      for(let i = godine.length - 1; i >= 0; i--) {
+        sablon += `<div class='col-md-3'>
+                      <h4>${godine[i].naziv}</h4>
+                      <h4>${godine[i].godina}</h4>
+                      <img src='${godine[i].slika}'>
+                    </div>`;
+      }
+
+      $('#filmovi').html(sablon);
+    })
+
+    // sortiranje od a
+    $('#odA').on('click', function() {
+
+      const godine = [];
+
+      for(i = 0; i < data.length; i++) {
+        godine.push(data[i]);
+      }
+
+      function compare(a,b) {
+        if (a.naziv < b.naziv)
+          return -1;
+        if (a.naziv > b.naziv)
+          return 1;
+        return 0;
+      }
+      
+      godine.sort(compare);
+
+      let sablon = '';
+
+      for(let i = 0; i <godine.length; i++) {
+        sablon += `<div class='col-md-3'>
+                      <h4>${godine[i].naziv}</h4>
+                      <h4>${godine[i].godina}</h4>
+                      <img src='${godine[i].slika}'>
+                    </div>`;
+      }
+
+      $('#filmovi').html(sablon);
+    })
+
+    // sortiranje od z
+    $('#odZ').on('click', function() {
+
+      const godine = [];
+
+      for(i = 0; i < data.length; i++) {
+        godine.push(data[i]);
+      }
+
+      function compare(a,b) {
+        if (a.naziv < b.naziv)
+          return -1;
+        if (a.naziv > b.naziv)
+          return 1;
+        return 0;
+      }
+      
+      godine.sort(compare);
+
+      let sablon = '';
+
+      for(let i = godine.length - 1; i >= 0; i--) {
+        sablon += `<div class='col-md-3'>
+                      <h4>${godine[i].naziv}</h4>
+                      <h4>${godine[i].godina}</h4>
+                      <img src='${godine[i].slika}'>
+                    </div>`;
+      }
+
+      $('#filmovi').html(sablon);
     })
 })
