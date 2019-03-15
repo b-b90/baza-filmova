b4c0d7dd1b322b5960c8aa86ede22a0ecf894b96
b-b90 <b-b90@users.noreply.github.com>
2019-03-15 16:30:33 +0100

    comments

--- js/prikaz.js
+++ js/prikaz.js
@@ -3,6 +3,33 @@ var naslov = localStorage.getItem('naslov');
 var godina = localStorage.getItem('godina');
 var slika = localStorage.getItem('slika');
 
-document.querySelector(".ovde h4").innerHTML = naslov;
-document.querySelector(".ovde h5").innerHTML = godina;
-document.querySelector(".ovde img").src = slika;
+// document.querySelector(".ovde h4").innerHTML = naslov;
+// document.querySelector(".ovde h5").innerHTML = godina;
+// document.querySelector(".ovde img").src = slika;
+
+fetch('https://baza-filmova.herokuapp.com/filmovi/ ')
+  .then(res => res.json())
+  .then(data => {
+
+    for (let i = 0; i < data.length; i++) {
+
+        // console.log(data[i].komentari);
+
+        if(naslov === data[i].naziv) {
+            document.querySelector(".ovde h4").innerHTML = naslov;
+            document.querySelector(".ovde h5").innerHTML = godina;
+            document.querySelector(".ovde img").src = slika;
+
+            if(data[i].komentari != undefined || data[i].komentari != null) {
+
+                for (let j = 0; j < data[i].komentari.length; j++) {
+
+                    $('.ovde').append()
+
+                    console.log(data[i].komentari[j].user);
+                    console.log(data[i].komentari[j].comment);
+                }
+            }
+        }
+    }
+  })
